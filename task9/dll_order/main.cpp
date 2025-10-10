eb47a788faee4b74a104fb7373f2559731f4a43a
hasherezade <hasherezade@gmail.com>
2025-10-10 16:26:05 -0700

    [task9][order] Refactored the DLL order calculator

--- task9/dll_order/main.cpp
+++ task9/dll_order/main.cpp
@@ -13,6 +13,7 @@
 
 #include "util.h"
 #include "verif.h"
+#include "dll_deps.h"
 
 #define DLLS_MAX 10000
 
@@ -20,79 +21,13 @@ size_t g_load_counts[DLLS_MAX] = { 0 };
 
 std::map<DWORD, WORD> g_PosToDll;
 
-struct DllInfo
+
+void countLoaded(const std::set<WORD>& deps)
 {
-    WORD id;
-    std::set<WORD> deps;
-
-    void countLoaded() const
-    {
-        for (auto itr = deps.begin(); itr != deps.end(); ++itr) {
-            WORD dll = *itr;
-            g_load_counts[dll]++;
-        }
+    for (auto itr = deps.begin(); itr != deps.end(); ++itr) {
+        WORD dll = *itr;
+        g_load_counts[dll]++;
     }
-
-    void print() {
-        std::cout << "DLL: " << std::dec << id << std::endl;
-        /*for (auto itr = deps.begin(); itr != deps.end(); ++itr) {
-            std::cout << (*itr) << ", ";
-        }
-        std::cout << "\n";
-        */
-    }
-};
-
-bool parseDllInfo(const std::string& line, DllInfo &dllInfo) {
-
-    std::string leftPart, rightPart;
-
-    // Split the line into two parts: DLL id and dependencies
-    std::size_t arrowPos = line.find("->");
-    if (arrowPos == std::string::npos) {
-        return false;
-    }
-
-    // Extract the DLL id (before '->')
-    leftPart = line.substr(0, arrowPos);
-    dllInfo.id = static_cast<WORD>(std::atoi(leftPart.c_str()));
-
-    // Extract the dependencies (after '->')
-    rightPart = line.substr(arrowPos + 3); // Skip over "->"
-
-    // Parse the dependencies
-    std::stringstream ss(rightPart);
-    std::string dep;
-    while (std::getline(ss, dep, ',')) {
-        dllInfo.deps.insert(static_cast<WORD>(std::atoi(dep.c_str())));
-    }
-
-    return true;
-}
-
-size_t read_resolved(const std::string& inpFile, std::map<WORD, DllInfo> &dllInfos)
-{
-    std::ifstream infile(inpFile);  // open file for reading
-    if (!infile.is_open()) {
-        std::cerr << "Error: could not open file.\n";
-        return 0;
-    }
-    size_t count = 0;
-    std::string line;
-    while (std::getline(infile, line)) {
-        count++;
-        
-        if (line.find("->") == std::string::npos) {
-            continue;
-        }
-        DllInfo info;
-        parseDllInfo(line, info);
-
-        dllInfos[info.id] = info;
-        
-    }
-    infile.close();
-    return count;
 }
 
 int main(int argc, char* argv[])
@@ -103,16 +38,16 @@ int main(int argc, char* argv[])
         return 0;
     }
     
-    std::map<WORD, DllInfo> dllInfos;
-
     char* input_file = argv[1];
     size_t input_size = 0;
-    read_resolved(input_file, dllInfos);
+
+    std::map<WORD, DllInfo> dllInfos;
+    read_dependencies(input_file, dllInfos);
     
     DWORD* verif = (DWORD*)g_VerifBuf;
     for (auto itr = dllInfos.begin(); itr != dllInfos.end(); ++itr) {
         const DllInfo info = itr->second;
-        info.countLoaded();
+        countLoaded(info.deps);
     }
     std::set<WORD> solved;
 
@@ -137,14 +72,18 @@ int main(int argc, char* argv[])
         }
         //std::cout << "Count: " << counter << std::endl;
     }
-    bool isFirst = true;
+
+    size_t i = 0;
     std::cout << "{";
-    for (auto itr = g_PosToDll.begin(); itr != g_PosToDll.end(); ++itr) {
-        if (!isFirst) {
+    for (auto itr = g_PosToDll.begin(); itr != g_PosToDll.end(); ++itr, ++i) {
+        if (i % 255 == 0) {
+            std::cout << "\n";
+        }
+        std::cout << itr->second;
+        if (i < (DLLS_MAX - 1)) {
             std::cout << ", ";
         }
-        isFirst = false;
-        std::cout << itr->second;
+
         //std::cout << itr->first << " : " << itr->second << std::endl;
     }
     std::cout << "};";
