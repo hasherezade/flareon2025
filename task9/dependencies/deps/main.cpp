4d27ed6a32a704ffb30d06760f68d7d3a843fa9b
hasherezade <hasherezade@gmail.com>
2025-10-11 05:55:51 -0700

    [task9][dependencies] Added code of dependencies listing

--- task9/dependencies/deps/main.cpp
+++ task9/dependencies/deps/main.cpp
@@ -7,6 +7,7 @@
 #include <peconv.h> // include libPeConv header
 
 #include "util.h"
+#define MAX_DLLS 10000
 
 bool getDllId(const std::string& dll_name, WORD &id)
 {
@@ -80,6 +81,46 @@ public:
     dll_deps& my_deps;
 };
 
+
+int populateLayer(std::map<WORD, dll_deps*> &dllToDeps, WORD mainID)
+{
+    dll_deps* d = dllToDeps[mainID];
+    size_t initialSize = d->deps.size();
+    bool collected[MAX_DLLS] = { 0 };
+    for (auto itr = d->deps.begin(); itr != d->deps.end(); ++itr) {
+        WORD next = *itr;
+        for (auto k = dllToDeps[next]->deps.begin(); k != dllToDeps[next]->deps.end(); ++k) {
+            collected[*k] = true;
+        }
+    }
+    for (WORD i = 0; i < MAX_DLLS; i++) {
+        if (collected[i]) {
+            d->deps.insert(i);
+        }
+    }
+
+    return (d->deps.size() - initialSize);
+}
+
+bool dump_layer_to_file(std::string out_file, int layer, std::map<WORD, dll_deps*> &dllToDeps)
+{
+    std::stringstream ss; 
+    ss << out_file << "." << std::dec << layer << ".txt";
+
+    std::ofstream oFile(ss.str());  // open file for reading
+    if (!oFile.is_open()) {
+        std::cerr << "Error: could not open file.\n";
+        return 0;
+    }
+
+
+    for (auto itr = dllToDeps.begin(); itr != dllToDeps.end(); ++itr) {
+        dll_deps* dep = itr->second;
+        dep->print(oFile);
+    }
+    oFile.close();
+}
+
 int main(int argc, char *argv[])
 {
     if (argc < 3) {
@@ -89,17 +130,9 @@ int main(int argc, char *argv[])
     std::string input_dir = argv[1];
     std::string out_file = argv[2];
 
-    std::ofstream oFile(out_file);  // open file for reading
-    if (!oFile.is_open()) {
-        std::cerr << "Error: could not open file.\n";
-        return 0;
-    }
-
-    const size_t chunks_max = 10000;
-
     std::map<WORD, dll_deps*> dllToDeps;
 
-    for (WORD id = 0; id < chunks_max; id++) {
+    for (WORD id = 0; id < MAX_DLLS; id++) {
         std::stringstream ss;
         ss << input_dir << "\\"
             << std::setw(4) << std::setfill('0') << id
@@ -117,13 +150,21 @@ int main(int argc, char *argv[])
             continue;
         }
         peconv::free_pe_buffer(mainMod);
-
     }
-    for (auto itr = dllToDeps.begin(); itr != dllToDeps.end(); ++itr) {
-        dll_deps* dep = itr->second;
-        dep->print(oFile);
+    std::cout << "First layer populated...\n";
 
+   for (size_t i = 1; true; ++i) {
+        size_t added = 0;
+        for (auto itr = dllToDeps.begin(); itr != dllToDeps.end(); ++itr) {
+            WORD id = itr->first;
+            if (id % 1000 == 0) {
+                std::cout << id << " Total added: " << added << "\n";
+            }
+            added += populateLayer(dllToDeps, itr->first);
+        }
+        std::cout << "Layer "  << i << " populated. " << " Added: " << added << "\n";
+        dump_layer_to_file(out_file, i, dllToDeps);
+        if (added == 0) break;
     }
-    oFile.close();
     return 0;
 }
